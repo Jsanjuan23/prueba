17def42f478d829d0a88cccf2d580464cb96bd70
El coco <jesussanjuan23@gmail.com>
2022-11-01 15:11:48 -0500

    agregando configuracion de bd

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB7BBnZqsYhyzkxH0YOod-j7w3nVNz8sLg",
+    authDomain: "medical-231cb.firebaseapp.com",
+    projectId: "medical-231cb",
+    storageBucket: "medical-231cb.appspot.com",
+    messagingSenderId: "409573091146",
+    appId: "1:409573091146:web:b3c671aa217229eefe83b3"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export{firebase}
+
+  
